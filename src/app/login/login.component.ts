a74b00ef22350aa622b4b02a56b9dfd036152886
lilingling <1192246772@qq.com>
2017-05-21 22:39:07 +0800

    'login'

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,14 +8,29 @@ import { AuthService } from '../core/auth.service';
 })
 export class LoginComponent implements OnInit {
   private text = 'hello';
+  private username:string;
+  private password:string;
+
   constructor(
     private authService : AuthService
   ) { }
 
   ngOnInit() {
+    this.username = '请输入用户名';
+    this.password = '请输入密码';
   }
 
-  onClick(username,password){
-     console.log('result   '+ this.authService.loginWithCredentials(username,password));
+  // onClick(){
+  //    console.log('result   '+ this.authService.loginWithCredentials(this.username,this.password));
+  // }
+
+  usernameFocus(){
+    this.username = ''
+  }
+  passwordFocus(){
+    this.password = ''
+  }
+  onSubmit(value){
+    console.log(this.authService.loginWithCredentials(value.login.username,value.login.password));
   }
 }
